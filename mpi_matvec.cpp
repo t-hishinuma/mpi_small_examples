f50aee4bb872741290a2f013350f0b58b8b60dd1
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2023-01-20 17:06:17 +0900

    small hello world

--- mpi_matvec.cpp
+++ mpi_matvec.cpp
@@ -1,6 +1,23 @@
-#include<iostream>
-#include<mpi.h>
+#include <mpi.h>
 
-int main(){
-	std::cout << "hello" << std::endl;
+#include <iostream>
+#include <vector>
+
+void generate_matrix() {}
+
+void matvec() {
+    int a = 1;
+    int b = 2;
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    std::cout << "hello" << rank << "," << size << std::endl;
+    std::vector<double> M = {1, 2, 3, 4};
+    MPI_Finalize();
 }
