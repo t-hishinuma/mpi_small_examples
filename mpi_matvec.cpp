5ef0c141173c1ca6a48b1abd2f7bc52029c9208b
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2023-01-20 17:38:12 +0900

    2nd commit

--- mpi_matvec.cpp
+++ mpi_matvec.cpp
@@ -3,10 +3,14 @@
 #include <iostream>
 #include <vector>
 
+// for debug
+void print_matrix(double* Mat, size_t M, size_t N, int rank);
+
 // MxN partitioned matrix
-void generate_matrix(double* M, size_t M, size_t N) {
+void generate_matrix(double* Mat, size_t M, size_t N, int rank, int size) {
     for (auto i = 0; i < M; i++) {
-        for (auto i = 0; i < M; i++) {
+        for (auto j = 0; j < N; j++) {
+            Mat[i * N + j] = rand();
         }
     }
 }
@@ -17,12 +21,22 @@ void matvec() {
 
 int main(int argc, char* argv[]) {
     int rank, size;
-
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     std::cout << "hello" << rank << "," << size << std::endl;
-    std::vector<double> M = {1, 2, 3, 4};
+    std::vector<double> Mat(rank * size);
+    std::vector<double> Vec(size);
     MPI_Finalize();
+}
+
+// debug util////////////////////////
+
+void print_matrix(double* Mat, size_t M, size_t N, int rank) {
+    for (auto i = 0; i < M; i++) {
+        for (auto j = 0; j < N; j++) {
+            std::cout << Mat[i * N + j] << std::endl;
+        }
+    }
 }
