f5d8fab447e5271c973c78e69e2431ed111ac7a1
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2023-01-24 10:21:23 +0900

    update hello world

--- 1_helloworld/mpi_hello.cpp
+++ 1_helloworld/mpi_hello.cpp
@@ -11,5 +11,24 @@ int main(int argc, char* argv[]) {
 
     std::cout << "hello world, my rank = " << rank << "/" << size << std::endl;
 
+    int data = 0;
+
+    MPI_Status stat;
+
+    if (rank == 0) {
+        data = rank;
+        MPI_Send(&data, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+        std::cout << "data is " << data << ", my rank is " << rank << std::endl;
+    } else if (rank == size - 1) {
+        MPI_Recv(&data, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &stat);
+        data++;
+        std::cout << "data is " << data << ", my rank is " << rank << std::endl;
+    } else {
+        MPI_Recv(&data, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &stat);
+        data++;
+        MPI_Send(&data, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+        std::cout << "data is " << data << ", my rank is " << rank << std::endl;
+    }
+
     MPI_Finalize();
 }
